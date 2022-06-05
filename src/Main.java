b159325aaca3e0b87c70b097b2b5b5104ea5c3db
cami-cohete <esquivca@mail.uc.edu>
2022-06-05 15:07:12 -0400

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	double cardBalance = 5000.00;
+    double intrerestRate = 0.17;
+    double cardInterest = 0;
+    cardInterest = cardBalance * intrerestRate;
+    System.out.println("If your card balance is $" + cardBalance);
+    System.out.printf("Then after one month your interest rate is $%.2f\n" ,cardInterest);
+    cardBalance = cardInterest + cardBalance;
+    cardInterest = cardBalance * intrerestRate;
+    System.out.printf("Your interest rate after two months is $%.2f\n" ,cardInterest);
     }
 }
